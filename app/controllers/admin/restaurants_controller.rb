493fda10e11f94a60ff7fb54f89f16cd12b56f9f
caseydyps <caseydyps@gmail.com>
2017-12-19 23:37:35 +0800

    add restaurant show action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -8,9 +8,47 @@ class Admin::RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+before_action :set_restaurant, only: [:show, :edit, :update]
+
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "restaurant was successfully updated"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      flash.now[:alert] = "restaurant was failed to update"
+      render :edit
+    end
+  end
+  
+  private
+   #其他程式碼
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 
   
 end
